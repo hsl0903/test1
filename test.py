246830b08008087aeebf7483e2dd79bced5a1a90
hanshuli <2412054914@qq.com>
2024-07-02 11:27:37 +0800

    test3

--- test.py
+++ test.py
@@ -508,3 +508,9 @@ my_dict = {'a': 1, 'b': 2, 'c': 3}
 
 for key, value in my_dict.items():
     print("Key:", key, "Value:", value)
+
+for key, value in my_dict.items():
+    print("Key:", key, "Value:", value)
+
+my_dict = {'a': 1, 'b': 2, 'c': 3}
+
