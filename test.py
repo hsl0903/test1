3acf56e9e7ec60fe0e9dd32ad6dc39555dddc83b
hanshuli <2412054914@qq.com>
2024-07-04 18:51:21 +0800

    1

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+
 def generate(numRows):
     triangle = []
 
@@ -390,17 +391,3 @@ my_dict = {'a': 1, 'b': 2, 'c': 3}
 
 for key, value in my_dict.items():
     print("Key:", key, "Value:", value)
-my_dict = {'a': 1, 'b': 2, 'c': 3}
-
-for key, value in my_dict.items():
-    print("Key:", key, "Value:", value)
-
-
-my_dict = {'a': 1, 'b': 2, 'c': 3}
-
-for key, value in my_dict.items():
-    print("Key:", key, "Value:", value)
-
-Key: a Value: 1
-Key: b Value: 2
-Key: c Value: 3
